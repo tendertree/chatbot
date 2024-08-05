4e0002ecde24a5ae99165ee0694d5b73fdde7e89
tendertree <tendertree@gmail.com>
2024-08-05 12:03:51 +0900

    feat: add supabase client

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,8 +1,6 @@
 import OpenAI from "openai";
 
-// Assign API key to variable
 const apiKey = process.env.OPEN_AI_KEY;
-// Initialise OpenAI API
 const openai = new OpenAI({ apiKey: apiKey });
 
 
@@ -20,14 +18,9 @@ export async function POST(req: Request) {
                 content: question,
             },
         ],
-        // We choose the model we want to use for our chatbot
         model: "gpt-3.5-turbo",
-        // We add a value for max_tokens to ensure the response won't exceed 300 tokens
-        // This is to make sure the responses aren't too long
         max_tokens: 300,
     });
-    // Then we return the response we receive from OpenAI
-    // Note: This will only work once we set up our frontend logic
     return new Response(JSON.stringify(response));
 }
 
