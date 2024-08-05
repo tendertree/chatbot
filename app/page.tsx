24dbed74a1fad9780c0427ca2585edb668c25b0b
tendertree <tendertree@gmail.com>
2024-08-05 12:29:04 +0900

    feat: add supabase client

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,12 @@ export default function Home() {
     const chartData = [
         { month: "January", desktop: 186, mobile: 80 },
         { month: "February", desktop: 305, mobile: 200 },
-        { month: "March", desktop: 237, mobile: 120 },
-        { month: "April", desktop: 73, mobile: 190 },
-        { month: "May", desktop: 209, mobile: 130 },
-        { month: "June", desktop: 214, mobile: 140 },
     ]
     const chartConfig = {
-        desktop: {
-            label: "Desktop",
-            color: "#2563eb",
-        },
-        mobile: {
-            label: "Mobile",
-            color: "#60a5fa",
-        },
     } satisfies ChartConfig
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>hello owld</div>
+            <div>Simple web pages</div>
 
         </main>)
 }
--- src/server/supabase.ts
+++ src/server/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { log } from "console";
 import { v4 as uuidv4 } from 'uuid'
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseKey = process.env.SUPABASE_ROLE_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ROLE_KEY || "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
